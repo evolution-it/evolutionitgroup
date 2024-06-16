1278c4c8ee7289dc0c653bce1ee51cad14937f4a
kevincrawford <github@kevin-crawford.com>
2024-06-15 22:51:57 -0400

    add contact form functionality

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,49 @@
+import sendgrid, { MailDataRequired } from "@sendgrid/mail";
+import { NextResponse } from "next/server";
+
+const key = process.env.SENDGRID_API_KEY || "";
+sendgrid.setApiKey(key);
+
+export async function POST(req: Request) {
+  const form: any = await req.json();;
+
+  console.log("form: ", form);
+
+  const responeHtml = `
+    <div style="padding-bottom: 16px; padding-top: 16px"><strong>RE: ${form.subject}</strong></div>
+    <div style="padding-bottom: 16px">${form.name},</div>
+    <div style="padding-bottom: 16px">Thanks for getting In touch. Your opinion is important to us. We will review your comments and get back to you soon. We typically respond within 48 hours.</div>
+    <div style="padding-bottom: 16px"><strong>Evolution IT Group</strong</div>
+  `;
+
+  const notification = `
+    <div style="padding-bottom: 16px; padding-top: 16px"><strong>New Contact:</strong></div>
+    <div style="padding-bottom: 16px">${form.name}< /br><a href="#"${form.email}</a>< /br><a href="#"${form.number}</a>< /br></div>
+    <div style="padding-bottom: 16px"><strong>Subject: ${form.subject}</strong></div>
+    <div style="padding-bottom: 16px"><strong>Message:</strong> >${form.message}</div>
+  `;
+
+  const memberEmail: MailDataRequired = {
+    to: form.email,
+    from: "info@evolutionitgroup.com",
+    subject: "Evolution IT Group",
+    html: responeHtml,
+  };
+
+  const notificationEmail: MailDataRequired = {
+    to: "info@evolutionitgroup.com",
+    from: "info@evolutionitgroup.com",
+    subject: "New Contact",
+    html: notification,
+  };
+
+  try {
+    await sendgrid.send(memberEmail);
+    await sendgrid.send(notificationEmail);
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: true, message: "error" });
+  }
+
+  return NextResponse.json({ error: false, message: "no error" });
+}
--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -2,6 +2,8 @@
 
 import React, { useState } from "react";
 import Image from "next/image";
+import Link from 'next/link';
+import validator from "validator";
 
 import contactImg from "/public/images/evolution-form-bg.png";
 
@@ -15,17 +17,65 @@ const ContactForm: React.FC = () => {
     agreeTerms: true,
   });
 
+  const [formValid, setFormValid] = useState(false);
+  const [formSent, setFormSent] = useState(false);
+  const [buttonText, setButtonText] = useState("Send Message");
+
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
+    const fd = Object.assign({ ...formData, [name]: value });
+    const isValidForm = handleValidation(fd);
+    setFormValid(isValidForm);
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleValidation = (fd: any): boolean => {
+    let isValid = true;
+
+    const isValidPhoneNumber = validator.isMobilePhone(fd.number);
+    const isValidEmail = validator.isEmail(fd.email);
+
+    if (fd.name.length <= 3) {
+      isValid = false;
+    }
+    if (fd.email.length <= 0 || !isValidEmail) {
+      isValid = false;
+    }
+    if (fd.number.length <= 0 || !isValidPhoneNumber) {
+      isValid = false;
+    }
+
+    if (fd.subject.length <= 3) {
+      isValid = false;
+    }
+    if (fd.message.length <= 24) {
+      isValid = false;
+    }
+
+    return isValid;
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
+    setButtonText("Sending Message");
     // Handle form submission logic here
-    console.log("Form data submitted:", formData);
+    const res = await fetch("/api/contact", {
+      body: JSON.stringify(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    const { error } = await res.json();
+
+    if (error) {
+      setButtonText("Send Message");
+    } else {
+      setFormSent(true);
+    }
   };
 
   return (
@@ -39,91 +89,111 @@ const ContactForm: React.FC = () => {
 
           <div className="row align-items-center">
             <div className="col-lg-6 col-md-12 p-3">
-              <Image src={contactImg} alt="image" width={685} height={458} />
+              <Image
+                src={contactImg}
+                alt="image"
+                width={685}
+                height={458}
+                priority={false}
+              />
             </div>
 
-            <div className="col-lg-6 col-md-12">
-              <form onSubmit={handleSubmit}>
-                <div className="row">
-                  <div className="col-lg-12 col-md-12">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        name="name"
-                        placeholder="Your Name"
-                        className="form-control"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                      />
+            {formSent ? (
+              <div className="col-lg-6 col-md-12 text-center">
+                <h2 className="pt-5">Thanks for getting in touch.</h2>
+                <div>We&apos;ll get back to you soon.</div>
+                <div><Link className="btn btn-primary mt-3" href="/">Back to Home</Link></div>
+              </div>
+            ) : (
+              <div className="col-lg-6 col-md-12">
+                <form onSubmit={handleSubmit}>
+                  <div className="row">
+                    <div className="col-lg-12 col-md-12">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          name="name"
+                          placeholder="Your Name"
+                          className="form-control"
+                          value={formData.name}
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-12 col-md-12">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          name="email"
+                          placeholder="Your email address"
+                          className="form-control"
+                          value={formData.email}
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6 col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          name="number"
+                          placeholder="Your phone number"
+                          className="form-control"
+                          value={formData.number}
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-6 col-md-6">
+                      <div className="form-group">
+                        <input
+                          type="text"
+                          name="subject"
+                          placeholder="Your Subject"
+                          className="form-control"
+                          value={formData.subject}
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-12 col-md-12">
+                      <div className="form-group">
+                        <textarea
+                          name="message"
+                          cols={30}
+                          rows={5}
+                          placeholder="Write your message..."
+                          className="form-control"
+                          value={formData.message}
+                          onChange={handleChange}
+                          required
+                        />
+                      </div>
+                    </div>
+
+                    <div className="col-lg-12 col-sm-12">
+                      <button
+                        type="submit"
+                        className={`btn btn-primary${
+                          formValid ? "" : " disabled"
+                        }`}
+                        disabled={!formValid}
+                      >
+                        {buttonText}
+                      </button>
                     </div>
                   </div>
-
-                  <div className="col-lg-12 col-md-12">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        name="email"
-                        placeholder="Your email address"
-                        className="form-control"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-lg-6 col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        name="number"
-                        placeholder="Your phone number"
-                        className="form-control"
-                        value={formData.number}
-                        onChange={handleChange}
-                        required
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-lg-6 col-md-6">
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        name="subject"
-                        placeholder="Your Subject"
-                        className="form-control"
-                        value={formData.subject}
-                        onChange={handleChange}
-                        required
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-lg-12 col-md-12">
-                    <div className="form-group">
-                      <textarea
-                        name="message"
-                        cols={30}
-                        rows={5}
-                        placeholder="Write your message..."
-                        className="form-control"
-                        value={formData.message}
-                        onChange={handleChange}
-                        required
-                      />
-                    </div>
-                  </div>
-
-                  <div className="col-lg-12 col-sm-12">
-                    <button type="submit" className="btn btn-primary">
-                      Send Message
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
+                </form>
+              </div>
+            )}
           </div>
         </div>
       </div>
